58a7cbc758cdbea39de34e7ec2f0790be4529608
totallytom <ilovekimchi2143@gmail.com>
2024-09-19 15:49:27 -0400

    calendar

--- Profile/dateUpdater.js
+++ Profile/dateUpdater.js
@@ -4,11 +4,15 @@ const dayNumE1 = document.getElementById("day-number");
 const yearE1 = document.getElementById("year");
 const date = new Date();
 const month = date.getMonth();
+const day = date.getDate();
 monthNameE1.innerText = date.toLocaleString("en", {
     month: "long"
 }); 
 dayNameE1.innerText = date.toLocaleString("en",{
     weekday: "long"
 });
+dayNumE1.innerText = date.toLocaleString("en", {
+    day: "numeric"
+})
 dayNumE1.innerText = date.getDate()
 yearE1.innerText = date.getFullYear()
