c53e209389f9e924d727cd0a8713b05f9b5a27c1
totallytom <ilovekimchi2143@gmail.com>
2024-09-19 20:08:13 -0400

    change layout for date

--- Profile/dateUpdater.js
+++ Profile/dateUpdater.js
@@ -2,13 +2,16 @@ const monthNameE1 = document.getElementById("month-name");
 const dayNameE1 = document.getElementById("day-name");
 const dayNumE1 = document.getElementById("day-number");
 const yearE1 = document.getElementById("year");
+
 const date = new Date();
-const month = date.getMonth();
+
+const month = date.getMonth(); 
 const day = date.getDate();
+const year = date.getFullYear();
 monthNameE1.innerText = date.toLocaleString("en", {
     month: "long"
 }); 
-dayNameE1.innerText = date.toLocaleString("en",{
+dayNumE1.innerText = date.toLocaleString("en",{
     weekday: "long"
 });
 dayNumE1.innerText = date.getDate()
