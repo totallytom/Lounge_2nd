5ec177765fd97de8ddbf8ad452e54e9a28f086b1
totallytom <ilovekimchi2143@gmail.com>
2024-09-23 15:48:30 -0400

    posts

--- Posts/Posts.js
+++ Posts/Posts.js
@@ -4,3 +4,8 @@ import ReactDOM from "react-dom";
 document.getElementById("page").innerHTML = page
 const page = " Posts"; 
 
+document.getElementById("sidemenu").innerHTML = sidemenu
+const sidemenu = " Sidemenu";
+
+document.getElementById("menupage").innerHTML = menupage
+const menupage = " Menupage"; 
