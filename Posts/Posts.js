94a83104512625060f9990ab0dc5b6238c478ab6
totallytom <ilovekimchi2143@gmail.com>
2024-10-26 09:49:44 -0400

    new

--- Posts/Posts.js
+++ Posts/Posts.js
@@ -1,11 +1,31 @@
-import react from "react";
-import ReactDOM from "react-dom";
+var settingsmenu = document.querySelector(".settings-menu");
+var darkBtn = document.getElementById("dark-btn");
 
-document.getElementById("page").innerHTML = page
-const page = " Posts"; 
+function settingsMenuToggle() {
+    settingsmenu.classList.toggle("settings-menu-height");
+}
 
-document.getElementById("sidemenu").innerHTML = sidemenu
-const sidemenu = " Sidemenu";
+darkBtn.onclick = function() {
+    darkBtn.classList.toggle("dark-btn-on");
+    document.body.classList.toggle("dark-theme");
 
-document.getElementById("menupage").innerHTML = menupage
-const menupage = " Menupage"; 
+    if (localStorage.getItem("theme") == "light") {
+        localStorage.setItem("theme", "dark");
+    }
+    else {
+        localStorage.setItem("theme", "light");
+    }
+}
+
+
+if (localStorage.getItem("theme") == "light") {
+    darkBtn.classList.remove("dark-btn-on");
+    document.body.classList.remove("dark-theme");
+}
+else if(localStorage.getItem("theme") == "dark") {
+    darkBtn.classList.add("dark-btn-on");
+    document.body.classList.add("dark-theme");
+}
+else{
+    localStorage.setItem("theme", "light");
+}
