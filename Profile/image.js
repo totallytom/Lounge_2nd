25819f90b1490ca5791d6133d80e2c345f5ed218
totallytom <ilovekimchi2143@gmail.com>
2024-11-01 22:05:22 -0400

    changes

--- Profile/image.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector(".menu-container");
-const picture = document.querySelector(".picture");
-
-menu.addEventListener("click", () => {
-    console.log("Element was clicked");
-
-});
